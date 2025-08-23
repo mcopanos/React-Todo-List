3620c03cafcc485705b07ea2d30088f4cf0143f5
Michael Copanos <mcopanos@gmail.com>
2025-08-22 19:49:30 -0500

    React with Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-center'>
+        <h1 className='text-emerald-100'>React and Tailwind CSS Template</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
