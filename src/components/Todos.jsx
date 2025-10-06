c47dcc566d0f61a4aeb350695a8ccfd359b31140
Michael Copanos <mcopanos@gmail.com>
2025-10-05 23:00:05 -0500

    todo checkbox on change event

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+
 export default function Todos({todos}) {
+    const [isChecked, setIsChecked] = useState(false);
+
+
     return (
         <ul className="w-xl h-full flex flex-col gap-3 mx-auto">
             {todos.map(todo => (
-                    <div className="flex justify-between w-full p-6 rounded-xl shadow-lg dark:bg-white/6 dark:shadow-2xl">
-                        <input type="radio" />
+                    <div className={!isChecked 
+                                    ? "flex justify-between w-full p-6 rounded-xl shadow-lg dark:bg-white/6 dark:shadow-2xl"
+                                    : "line-through flex justify-between w-full p-6 rounded-xl shadow-lg dark:bg-white/6 dark:shadow-2xl"
+                                    }
+                    >
+                        <input 
+                            type="checkbox" 
+                            name={todo.name} 
+                            value={todo.name} 
+                            onChange={(e) => setIsChecked(e.target.checked)} 
+                        /> 
                         <li key={todo.id} className="text-xl capitalize">
-                            { todo.name }
+                            { todo.name } 
                         </li>
                         <button className='bg-violet-500 hover:bg-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700 rounded-sm w-18 h-10'>Edit</button>
                     </div>
