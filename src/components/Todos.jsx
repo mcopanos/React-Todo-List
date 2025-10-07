b5fe23701ef926f0c8c35a4797cb99dd40c37aba
Michael Copanos <mcopanos@gmail.com>
2025-10-06 20:52:37 -0500

    style(checkbox): add conditional styles to variables

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -2,16 +2,13 @@ import { useState } from "react";
 
 export default function Todos({todos}) {
     const [isChecked, setIsChecked] = useState(false);
-
+    const notCompletedStyle = "flex justify-between w-full p-6 rounded-xl shadow-lg dark:bg-white/6 dark:shadow-2xl"
+    const completedStyle = "line-through flex justify-between w-full p-6 rounded-xl shadow-lg dark:bg-white/6 dark:shadow-2xl"
 
     return (
         <ul className="w-xl h-full flex flex-col gap-3 mx-auto">
             {todos.map(todo => (
-                    <div className={!isChecked 
-                                    ? "flex justify-between w-full p-6 rounded-xl shadow-lg dark:bg-white/6 dark:shadow-2xl"
-                                    : "line-through flex justify-between w-full p-6 rounded-xl shadow-lg dark:bg-white/6 dark:shadow-2xl"
-                                    }
-                    >
+                    <div className={!isChecked ? notCompletedStyle : completedStyle} >
                         <input 
                             type="checkbox" 
                             name={todo.name} 
